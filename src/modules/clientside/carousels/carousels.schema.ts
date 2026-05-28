eb914915d799defab16827f409ae5ef00aed9fe9
Maxmillian <maximilianogbuabor@gmail.com>
2026-05-28 21:04:53 +0100

    style: change query namemax to limit + update carousel docs

--- src/modules/clientside/carousels/carousels.schema.ts
+++ src/modules/clientside/carousels/carousels.schema.ts
@@ -2,6 +2,6 @@ import * as z from "zod"
 
 export const carouselSchema = z.object({
     query: z.object({
-        max: z.coerce.number().int().min(18).max(99).default(36)
+        limit: z.coerce.number().int().min(18).max(99).default(36)
     })
 })
