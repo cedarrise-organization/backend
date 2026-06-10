81d98d8cf3c276afc13d1d26b708ce92c5c09a0e
Maxmillian <maximilianogbuabor@gmail.com>
2026-06-10 20:36:17 +0100

    refact: more cookie settings changes.. I'm sleepy

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -1,13 +1,13 @@
 export const accessCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "none",
   maxAge: 1000 * 60 * 15
 } as const; 
 
 export const refreshCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "none",
   maxAge: 1000 * 60 * 60 * 24 * 7
 } as const; 
