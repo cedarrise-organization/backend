efb4f50f03f2c9a901ecca50699fcc7daff593c9
Maxmillian <maximilianogbuabor@gmail.com>
2026-06-13 00:17:22 +0100

    chore: update docs to include response object + update cookie max-age

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -2,7 +2,7 @@ export const accessCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production", // should browser send the cookie over https?
   sameSite: "none", // allows us to send cookies across different domains
-  maxAge: 1000 * 60 * 15, // matches access token exp age
+  maxAge: 1000 * 60 * 60 * 24 * 7, // Long lived for retrieval 
 } as const;
 
 export const refreshCookieOptions = {
