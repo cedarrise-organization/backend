1f190b267e4538132757e3891d47000a3febb84d
Maxmillian <maximilianogbuabor@gmail.com>
2026-07-28 16:14:00 +0100

    fix: fix pool error

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,6 @@
-import { Pool } from "pg"
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import logger from "../configs/logger.config.js"
+import logger from "../configs/logger.config.js";
 
 const dbMap = new Map([
   ["development", process.env.PG_DATABASE_DEV_URL],
@@ -16,17 +16,18 @@ const pool = new Pool({
 
 export async function connectDatabase() {
   try {
-    await pool.connect();
-    logger.info("Connected to database Pool successfully")
+    const client = await pool.connect();
+    client.release();
+    logger.info("Connected to database Pool successfully");
   } catch (err) {
     logger.error("Failed to connect to database:", err);
     process.exit(1);
   }
 }
 
-pool.on("error", () => {
-  logger.error("Unexpected PostgreSQL pool error.")
+pool.on("error", (err, client) => {
+  logger.error("Unexpected error on idle client", { err });
 });
 
-const db = drizzle({client: pool})
+const db = drizzle({ client: pool });
 export default db;
